fe7f3d217e63273c59541ced1ea76a85aa2e8149
rhitikag <rhitikaganguli@gmail.com>
2025-02-19 18:42:15 +0530

    intial

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,8 +1,15 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -20,8 +27,37 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
 
     }
 
+
     @Override
     public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<SearchResult> carResults = null;
+        try {
+            carResults = Arrays.asList(
+                    mapper.readValue(
+                            getClass().getClassLoader().getResource("rental_cars.json"),
+                            SearchResult[].class
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<SearchResult> hotelResults = null;
+        try {
+            hotelResults = Arrays.asList(
+                    mapper.readValue(
+                            getClass().getClassLoader().getResource("hotels.json"),
+                            SearchResult[].class
+                    )
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
 
     }
 
